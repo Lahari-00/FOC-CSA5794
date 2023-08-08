158149cc25bdebbb1aa05ecb5a6927c42fd9694c
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2023-08-08 12:36:12 +0530

    Add files via upload

---
+++ series(12+22+----).cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,sum=0;
+	printf("enter the number: ");
+	scanf("%d",&n);
+	for(i=10;i<=n;i++)
+	{
+		if(i%10==2)
+		{
+		sum=sum+i;
+	    }
+	}
+	printf("sum of the series is: %d",sum);
+	return 0;
+}
