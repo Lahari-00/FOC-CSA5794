82d4ccf05bfc40e34e71fe70fdbfb9f4f99421a2
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2023-08-10 13:05:01 +0530

    Add files via upload

---
+++ square root.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float num,square;
+	printf("enter the number: ");
+	scanf("%f",&num);
+	square=sqrt(num);
+	printf("square root is: %f",square);
+}
