6e3d85f4fcead9dd212462ad0cf0a4744a875e6a
Lahari-00 <112242529+Lahari-00@users.noreply.github.com>
2023-08-14 09:27:55 +0530

    Add files via upload

---
+++ row add.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a[10][10],sum=0;
+	int r1,r2,c1,c2,i,j;
+	printf("no.of rows of mat:");
+	scanf("%d",&r1);
+	printf("no.of columns of mat: ");
+	scanf("%d",&c1);
+	printf("enter elements of matrix : ");
+	for(i=0;i<r1;i++)
+	{
+		for(j=0;j<c1;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r1;i++)
+	{
+		for(j=0;j<c1;j++)
+		{
+			sum=sum+a[i][j];
+		}
+	}
+	printf("%d",sum);
+}
